4b42ee8807b4cd168d59c04270a992cc330f969c
winnie1027 <71732749+winnie1027@users.noreply.github.com>
2021-03-23 14:01:41 -0500

    Imported Parse

---
+++ Campusgram/Pods/Target Support Files/Pods-CampusgramTests/Pods-CampusgramTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CampusgramTests : NSObject
+@end
+@implementation PodsDummy_Pods_CampusgramTests
+@end
---
+++ Campusgram/Pods/Target Support Files/Pods-CampusgramUITests/Pods-CampusgramUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CampusgramUITests : NSObject
+@end
+@implementation PodsDummy_Pods_CampusgramUITests
+@end
