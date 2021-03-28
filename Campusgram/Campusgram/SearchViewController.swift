36a4692b20662034c50094db2b8b5f13b70d7fc9
Kennedi Cosma <kencosma2020@outlook.com>
2021-03-28 15:30:55 -0400

    The changes I made were adding the app icon to the sign up screen, adding the icon logos to the asset file, creating the profile and search screens and the files to go with them

---
+++ Campusgram/Campusgram/SearchViewController.swift
@@ -0,0 +1,46 @@
+//
+//  SearchViewController.swift
+//  Campusgram
+//
+//  Created by Kennedi Cosma on 3/28/21.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    @IBOutlet weak var searBarContoller: UISearchBar!
+    
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    @IBOutlet weak var eventLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func timeButton(_ sender: Any) {
+    }
+    
+    @IBAction func locationButton(_ sender: Any) {
+    }
+    
+    
+    @IBAction func eventButton(_ sender: Any) {
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
