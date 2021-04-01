21885bbb25136d575fb9d2f72c0a7c48f6d7ed38
winnie1027 <71732749+winnie1027@users.noreply.github.com>
2021-03-31 23:44:44 -0500

    Added screen for making a new post

---
+++ Campusgram/Campusgram/NewPostViewController.swift
@@ -0,0 +1,42 @@
+//
+//  NewPostViewController.swift
+//  Campusgram
+//
+//  Created by Winnie Yang on 3/31/21.
+//
+
+import UIKit
+
+class NewPostViewController: UIViewController {
+
+    @IBOutlet weak var imageField: UIImageView!
+    
+    @IBOutlet weak var eventNameField: UITextField!
+    
+    @IBOutlet weak var hostNameField: UITextField!
+    
+    @IBOutlet weak var dateField: UITextField!
+    
+    @IBOutlet weak var locationField: UITextField!
+    
+    @IBOutlet weak var capacityField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
