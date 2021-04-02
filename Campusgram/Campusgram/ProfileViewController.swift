25ce71a05d11d31ba88cf239593f9c5b640f2a87
winnie1027 <71732749+winnie1027@users.noreply.github.com>
2021-04-01 20:36:19 -0500

    Added constraints to new post page & added code to profile

--- Campusgram/Campusgram/ProfileViewController.swift
+++ Campusgram/Campusgram/ProfileViewController.swift
@@ -6,12 +6,15 @@
 //
 
 import UIKit
+import AlamofireImage
+import Parse
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet weak var profileImage: UIImageView!
     
-    @IBOutlet weak var profileLabel: UILabel!
+    @IBOutlet weak var usernameLabel: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +22,29 @@ class ProfileViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func changeButton(_ sender: Any) {
+    @IBAction func changeProfilePicButton(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.sourceType = .camera
+        } else {
+            picker.sourceType = .photoLibrary
+        }
+        
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.editedImage] as! UIImage
+        
+        let size = CGSize(width: 300, height: 300)
+        let scaledImage = image.af_imageAspectScaled(toFill: size)
+        
+        imageView.image = scaledImage
+        
+        dismiss(animated: true, completion: nil)
     }
     
     @IBAction func changeNameButton(_ sender: Any) {
