fd4c1e76905c893d44586ea72f83b3c604d7f9ea
winnie1027 <71732749+winnie1027@users.noreply.github.com>
2021-04-01 20:08:09 -0500

    Connected making a new post page to parse

--- Campusgram/Campusgram/NewPostViewController.swift
+++ Campusgram/Campusgram/NewPostViewController.swift
@@ -6,8 +6,10 @@
 //
 
 import UIKit
+import AlamofireImage
+import Parse
 
-class NewPostViewController: UIViewController {
+class NewPostViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
 
     @IBOutlet weak var imageField: UIImageView!
     
@@ -28,7 +30,55 @@ class NewPostViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func onPostButton(_ sender: Any) {
+        let post = PFObject(className: "Posts")
+        
+        post["name"] = eventNameField.text!
+        post["host"] = hostNameField.text!
+        post["date"] = dateField.text!
+        post["location"] = locationField.text!
+        post["capacity"] = capacityField.text!
+        
+        let imageData = imageField.image!.pngData()
+        let file = PFFileObject(name: "image.png", data: imageData!)
+        
+        post["image"] = file
+        
+        post.saveInBackground { (success, error) in
+            if success {
+                self.dismiss(animated: true, completion: nil)
+                print("Saved!")
+            } else {
+                print("Error!")
+            }
+        }
+    }
+    
+    @IBAction func onCameraButton(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.sourceType = .camera
+        } else {
+            picker.sourceType = .photoLibrary
+        }
+        
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.editedImage] as! UIImage
+        
+        let size = CGSize(width: 300, height: 300)
+        let scaledImage = image.af_imageAspectScaled(toFill: size)
+        
+        imageField.image = scaledImage
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
     /*
     // MARK: - Navigation
 
