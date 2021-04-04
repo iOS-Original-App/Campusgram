55b98ccb5cdfa7f2d56e6bd74bafc3f1044e2580
shanepatra <shanepatra@gmail.com>
2021-04-04 22:22:55 +0700

    Added saved event screen

---
+++ Campusgram/Campusgram/SavedViewController.swift
@@ -0,0 +1,76 @@
+//
+//  SavedViewController.swift
+//  Campusgram
+//
+//  Created by Shane Patra on 3/28/21.
+//
+
+import UIKit
+import Parse
+import AlamofireImage
+
+class SavedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var parties = [PFObject]()
+    var selectedParty: PFObject!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let query = PFQuery(className: "Parties")
+        query.includeKeys(["host", "date", "party", "joined"])
+    
+        query.findObjectsInBackground{ (parties, error) in
+            if parties != nil {
+                self.parties = parties!
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return parties.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let party = parties[indexPath.section]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PartyCell") as! PartyCell
+        
+        let user = party["host"] as! PFUser
+            
+        cell.hostLabel.text = user.username
+        cell.partyLabel.text = party["party"] as? String
+        cell.dateLabel.text = party["date"] as? String
+        cell.joinedLabel.text = party["joined"] as? String
+        
+        let imageFile = party["image"] as! PFFileObject
+        let urlString = imageFile.url!
+        let url = URL(string: urlString)!
+        
+        cell.partyView.af_setImage(withURL: url)
+        
+        return cell
+
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
