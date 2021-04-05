1bbd5421d0d66b8930808405df1697eeb3f4163b
Kennedi Cosma <kencosma2020@outlook.com>
2021-04-05 17:35:07 -0400

    I did the search, map, and chat screens

---
+++ Campusgram/Campusgram/ChatCell.swift
@@ -0,0 +1,23 @@
+//
+//  ChatCell.swift
+//  Campusgram
+//
+//  Created by Kennedi Cosma on 4/5/21.
+//
+
+import UIKit
+
+class ChatCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Campusgram/Campusgram/SettingsViewController.swift
@@ -0,0 +1,45 @@
+//
+//  SettingsViewController.swift
+//  Campusgram
+//
+//  Created by Kennedi Cosma on 4/5/21.
+//
+
+import UIKit
+import Parse
+
+
+class SettingsViewController: UIViewController {
+    
+    @IBOutlet weak var collegeTextField: UITextField!
+    
+    @IBOutlet weak var schoolTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onTap(_ sender: Any) {
+    }
+    
+    @IBAction func deleteAccountButton(_ sender: Any) {
+        PFUser.delete("loginsegue")
+    }
+    
+    @IBAction func logOutButton(_ sender: Any) {
+        PFUser.logOut()
+        
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let LoginViewController = main.instantiateViewController(identifier: "LoginViewController")
+        guard let windowscene = UIApplication.shared.connectedScenes.first as? UIWindowScene, let delegate = windowscene as? AppDelegate else { return }
+        
+        delegate.window?.rootViewController = LoginViewController
+        
+    
+
+}
+}
