e5f71e0a12f503c1ae630487eadb561c8c98ea84
shanepatra <shanepatra@gmail.com>
2021-04-11 16:23:15 +0700

    Fixed the saved event tab bar error

---
+++ Campusgram/Campusgram/SingleEventViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SingleEventViewController.swift
+//  Campusgram
+//
+//  Created by Shane Patra on 4/10/21.
+//
+
+import UIKit
+
+class SingleEventViewController: UIViewController {
+
+    @IBOutlet weak var partyView: UIImageView!
+    @IBOutlet weak var partyLabel: UILabel!
+    @IBOutlet weak var hostLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var joinedLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
